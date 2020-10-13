697dc3975965b737b5df8dc2c01992659a765ff3
TTG <ttg@TTGs-MacBook-Pro.local>
2020-10-13 15:52:51 +0100

    Fixing compilation errors

--- src/cutscene.c
+++ src/cutscene.c
@@ -1,23 +1,7 @@
 #include "include/cutscene.h"
 
-cutscene_event_T* text_event(int msg) {
-    return NULL;
-}
-
-cutscene_event_T* wait_event(int duration, animation_T* animation) {
-    return NULL;
-}
-
-cutscene_event_T* move_event(game_object_T* target, int x, int y) {
-    return NULL;
-}
-
-void do_event(cutscene_event_T* event) {
-
-}
-
 cutscene_T* cutscene(node_T* registry, bool autostep, bool allow_update, int count, ...) {
-    return NULL;
+
 }
 
 void step(cutscene_T* cutscene) {
--- src/include/cutscene.h
+++ src/include/cutscene.h
@@ -4,34 +4,13 @@
 #include "game_object.h"
 #include "../vendor/include/game.h"
 
-enum event_type {
-    TEXT,
-    WAIT,
-    MOVE
-};
-
 struct event_data {
-    game_object_T* target;
-    animation_T* animation;
-    int duration;
-    int x;
-    int y;
+    bool (*event) (game_T*, node_T*);
+    bool complete;
 };
-
-typedef struct CUTSCENE_EVENT {
-    enum event_type type;
-    void* data;
-    bool executed;
-} cutscene_event_T;
-
-cutscene_event_T* text_event(int msg);
-cutscene_event_T* wait_event(int duration, animation_T* animation);
-cutscene_event_T* move_event(game_object_T* target, int x, int y);
-
-void do_event(cutscene_event_T* event);
 
 typedef struct CUTSCENE {
-    cutscene_event_T** events;
+    struct event_data** events;
     bool autostep;
     bool allow_update;
 } cutscene_T;
