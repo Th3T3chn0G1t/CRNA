f4bdf312c024640e47c6ef42cf37729fbbfaf3ff
TTG <ttg@TTGs-MBP.lan>
2020-12-01 16:56:26 +0000

    Generated docs for README changes

--- docs/navtreedata.js
+++ docs/navtreedata.js
@@ -26,17 +26,19 @@ var NAVTREE =
 [
   [ "CRNA", "index.html", [
     [ "Tutorial", "md_res_docs_tutorial_welcome.html", [
+      [ "Documentation", "index.html#autotoc_md2", null ],
+      [ "Config", "index.html#autotoc_md3", null ],
       [ "Welcome", "md_res_docs_tutorial_welcome.html#autotoc_md1", null ]
     ] ],
     [ "Acknowledgments", "md__a_c_k_n_o_w_l_e_d_g_m_e_n_t_s.html", null ],
     [ "Contributing Guidelines", "md__c_o_n_t_r_i_b_u_t_i_n_g.html", [
-      [ "Code Style", "md__c_o_n_t_r_i_b_u_t_i_n_g.html#autotoc_md4", [
-        [ "Sources", "md__c_o_n_t_r_i_b_u_t_i_n_g.html#autotoc_md5", null ],
-        [ "Headers", "md__c_o_n_t_r_i_b_u_t_i_n_g.html#autotoc_md6", null ]
+      [ "Code Style", "md__c_o_n_t_r_i_b_u_t_i_n_g.html#autotoc_md6", [
+        [ "Sources", "md__c_o_n_t_r_i_b_u_t_i_n_g.html#autotoc_md7", null ],
+        [ "Headers", "md__c_o_n_t_r_i_b_u_t_i_n_g.html#autotoc_md8", null ]
       ] ],
-      [ "Scripts & Tools", "md__c_o_n_t_r_i_b_u_t_i_n_g.html#autotoc_md7", [
-        [ "Makefile", "md__c_o_n_t_r_i_b_u_t_i_n_g.html#autotoc_md8", null ],
-        [ "Other tools", "md__c_o_n_t_r_i_b_u_t_i_n_g.html#autotoc_md9", null ]
+      [ "Scripts & Tools", "md__c_o_n_t_r_i_b_u_t_i_n_g.html#autotoc_md9", [
+        [ "Makefile", "md__c_o_n_t_r_i_b_u_t_i_n_g.html#autotoc_md10", null ],
+        [ "Other tools", "md__c_o_n_t_r_i_b_u_t_i_n_g.html#autotoc_md11", null ]
       ] ]
     ] ],
     [ "Deprecated List", "deprecated.html", null ],
