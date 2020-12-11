4ef19b9fa9f650c04b0e0a6a21e22d24cf1726c7
Th3T3chn0G1t <bbanerjee@stmc.ie>
2020-12-11 19:12:02 +0000

    Updating date in licence info

--- src/engine/vendor/sdl_wrapper/color.c
+++ src/engine/vendor/sdl_wrapper/color.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2005 - Benjamin Banerjee <kyraliancitizen@gmail.com>
+ * Copyright (C) 2020 - Benjamin Banerjee <kyraliancitizen@gmail.com>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
